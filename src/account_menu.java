ea06cbb9b2a7ba1f3f7c8fdc8643c53e4b30d4b7
TaeUkChu <hippo2012@naver.com>
2021-03-16 00:06:48 +0900

    add account_menu

---
+++ src/account_menu.java
@@ -0,0 +1,16 @@
+
+public class account_menu {
+	public static void main(String[] args) {
+		System.out.println("****저축 계좌 관리 시스템에 오신 것을 환영합니다****");
+		System.out.println("*********원하시는 메뉴를 선택해 주세요*********");
+		
+		System.out.println("1.멤버 추가하기.");
+		System.out.println("2.멤버 삭제하기.");
+		System.out.println("3.멤버 조회하기.");
+		System.out.println("3-1. 선택한 멤버에 입금하기");
+		System.out.println("3-2. 선택한 멤버에서 출금하기");
+		System.out.println("3-3. 취소하고 메뉴로 돌아가기.");
+			
+	}
+
+}
